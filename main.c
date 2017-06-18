f63a1ee85a2b14d4c4b77b52f28419e15e178eb9
mathcruz <matheus.mcrz@gmail.com>
2017-06-17 21:19:10 -0300

    Update main.c
    Done!

--- main.c
+++ main.c
@@ -3,6 +3,17 @@
 #include <grafo.h>
 
 TG * criaGrafo(char * nomeArq);
+TG * salvaNo(TNo * no);
+int orientado(TG*g); 							//verifica se eh orientado (1) ou nao orientado(0)
+int conexo(TG * g);                             //verifica se eh conexo
+int colorir(TG * g);
+void descolorir(TG *g);     					//descolore colocando 0 como cor de todos os nos
+void coloreConexos(TG*g, TNo* no, int cor);     //colore o no e todos os grafos que ele consegue chegar
+int caminho(TG *g, TNo * no1, TNo * no2);       //verifica se existe um caminho entre um no e outro
+void fortementeConexo(TG *g);                   //imprime os componentes fortemente conexos
+void pontes(TG * g);						    //imprime as pontes do grafo
+void pontosDeArt(TG * g, TNo * no);
+
 
 int main(int argc, char* argv[])
 {
@@ -14,7 +25,8 @@ int main(int argc, char* argv[])
     int con = conexo(g);
     printf("dir: %d\nconexo: %d\n", dir, con);
     while(1){
-        printf("\n 1 - Inserir \n 2 - Retirar \n 3 - Buscar \n 4 - Imprimir \n 5 - Especial \n 6 - Sair\n");
+        con = conexo(g);
+        printf("\n MENU \n 1 - Inserir \n 2 - Retirar \n 3 - Buscar \n 4 - Imprimir \n 5 - Especial \n 6 - Sair\n Digite uma opcao: ");
         scanf("%d",&i);
 
         if(i == 6){
@@ -24,69 +36,87 @@ int main(int argc, char* argv[])
 
         switch(i){
 
-            case(1):
-                printf("\n 1 - No \n 2 - Aresta \n");
+            case(1):                                        //OPCAO INSERE
+                printf("\n 1 - No \n 2 - Aresta \n Digite uma opcao: ");
                 scanf("%d",&n);
-                if(n == 1){
-                    printf("\n Digite o valor do no:");
+                if(n == 1){                                 //INSERE NO
+                    printf("\n Digite o valor do no: ");
                     scanf("%d",&n);
                     insere_no(g, n);
                     if(!dir) colorir(g);
-                }else if(n == 2){
-                    printf("\n Digite valor de no1 e no2:");
+                    printf("\n %d inserido com sucesso!\n", n);
+                }else if(n == 2){                           //INSERE ARESTA
+                    printf("\n Digite valor de no1 e no2: ");
                     scanf("%d%d",&n,&r);
                     insere_aresta(g,n,r);
-                    if(dir == 0){
+                    TNo * no1 = busca_no(g, n);
+                    TNo * no2 = busca_no(g, r);
+                    if(dir == 0){                           //se grafo nao orientado
                         insere_aresta(g,r,n);
                         colorir(g);
                     }
+                    if(!no1 || !no2) printf("\n Impossivel inserir aresta entre %d e %d \n", n, r);
+                    else printf("\n Aresta entre %d e %d inserida com sucesso!\n",n,r);
                 }
                 break;
-            case(2):
-                printf("\n 1 - No \n 2 - Aresta \n");
+            case(2):                                        //OPCAO RETIRA
+                printf("\n 1 - No \n 2 - Aresta \n Digite uma opcao: ");
                 scanf("%d",&n);
-                if(n == 1){
-                    printf("\n Digite o valor do no:");
+                if(n == 1){                                 //RETIRA NO
+                    printf("\n Digite o valor do no: ");
                     scanf("%d",&n);
                     retira_no(g, n);
-                    if(dir==0) colorir(g);
-                }else if(n == 2){
-                    printf("\n Digite valor de no1 e no2:");
+                    if(dir==0) colorir(g);                  //se grafo nao orientado
+                    printf("\n %d removido com sucesso!\n",n);
+                }else if(n == 2){                   //RETIRA ARESTA
+                    printf("\n Digite valor de no1 e no2: ");
                     scanf("%d%d",&n,&r);
                     retira_aresta(g,n,r);
-                    if(dir == 0){
+                    if(dir == 0){                           //se grafo nao orientado
                         retira_aresta(g,r,n);
                         colorir(g);
                     }
+                    printf("\n Aresta entre %d e %d removida com sucesso!\n",n,r);
                 }
                 break;
-            case(3):
-                printf("\n 1 - No \n 2 - Aresta \n");
+            case(3):                                        //OPCAO BUSCA
+                printf("\n 1 - No \n 2 - Aresta \n Digite uma opcao: ");
                 scanf("%d",&n);
-                if(n == 1){
+                if(n == 1){                                 //BUSCA NO
                     printf("\n Digite o valor do no:");
                     scanf("%d",&n);
                     TNo *resp = busca_no(g, n);
-                    if(!resp) printf("Nó não encontrado");
-                    else printf("%d", resp->id_no);
-                }else if(n == 2){
+                    if(!resp) printf("\n %d nao pertence ao grafo.\n",n);
+                    else printf("\n %d pertence ao grafo.\n",n);
+                }else if(n == 2){                           //BUSCA ARESTA
                     printf("\n Digite valor de no1 e no2:");
                     scanf("%d%d",&n,&r);
                     TViz *resp = busca_aresta(g,n,r);
-                    if(!resp) printf("\n %d e %d nao estao conectados\n", &n, &r);
-                    else printf("\n %d e %d estao conectados\n", &n, &r);
+                    if(!resp) printf("\n %d e %d nao estao conectados.\n", n, r);
+                    else printf("\n %d e %d estao conectados.\n", n, r);
 
                 }
                 break;
 
-            case(4):
+            case(4):                                        //OPCAO IMPRIME
                 imprime(g);
                 break;
-            case(5):
-                pontes(g);
-                fortementeConexo(g);
-                //se orientado imprimir componentes fortemente conexas
-                //se nao orientado imprimir: se eh conectado (pontes e pontos de art); se nao conectado (componentes conectadas)
+            case(5):                                        //OPCAO ESPECIAL
+                if(dir == 1){                               //se grafo orientado
+                    printf("\n Grafo orientado\n Componentes fortemente conexas:\n");
+                    fortementeConexo(g);                    //imprime componentes fortemente conexas
+                }else{                                      //se nao orientado
+                    printf("\n Grafo nao orientado\n");
+                    if(con == 1){                           //se conectado
+                        printf("\n Pontes:\n");              //imprime pontes e pontos de articulacao
+                        pontes(g);
+                        printf("\n Pontos de articulacao:\n");
+                        pontosDeArt(g, g->prim);
+                    }else{                      //se nao conectado imprime componentes conexas
+                        printf("\n Compenentes conexos: \n");
+                        fortementeConexo(g);
+                    }
+                }
                 break;
 
         }
@@ -169,16 +199,16 @@ int conexo(TG * g){                             //verifica se eh conexo
 void fortementeConexo(TG *g){                   //imprime os componentes fortemente conexos
     int i, n = g->cores;
     TNo * no;
-    printf("componentes fortemente conexos:\n");
     for(i = 1; i<=n;i++){                       //para cada cor que existe no grafo...
         no = g->prim;
         while(no){								//imprime o no que tem aquela cor
-            if(no->cor == i) printf("[%d] ",no->id_no);
+            if(no->cor == i) printf(" [%d]",no->id_no);
             no = no->prox_no;
         }
         printf("\n");
     }
 }
+
 void pontes(TG * g){								//imprime as pontes do grafo
     TNo * no = g->prim;
     int conec = conexo(g);
@@ -195,13 +225,47 @@ void pontes(TG * g){								//imprime as pontes do grafo
                 conec = conexo(g);					//...verifica se o grafo continua conectado...
                 insere_aresta(g, no->id_no, temp);	//... e coloca as arestas de volta
                 insere_aresta(g, temp, no->id_no);
-                if(!conec) printf("ponte: %d e %d\n", no->id_no, temp);
-                									//se desconectou, imprime o no e o vizinho que causou isso
+                if(!conec && temp > no->id_no) printf(" %d e %d\n", no->id_no, temp);
+                                                    //se desconectou, imprime o no e o vizinho que causou isso
                 v = v->prox_viz;
             }while((v) && (v->id_viz != prim));
         }
         no = no->prox_no;
     }
+}
+
+void pontosDeArt(TG * g, TNo * no){
+    if(!g || !no) return;
+    pontosDeArt(g, no->prox_no);
+
+    TG * gAux = salvaNo(no);
+
+    int n = no->id_no;
+    retira_no(g, n);
+    if(!conexo(g)) printf(" %d\n", n);
+    insere_no(g,n);
+    TNo * noAux = busca_no(gAux, n);
+    TViz * v = noAux->prim_viz;
+    while(v){
+        insere_aresta(g, n, v->id_viz);
+        insere_aresta(g, v->id_viz, n);
+        v = v->prox_viz;
+    }
+    libera(gAux);
+}
+
+TG * salvaNo(TNo * no){
+    TG * g2 = cria();
+    insere_no(g2,no->id_no);
+    TNo * copia = g2->prim;
+
+    TViz * v = no->prim_viz;
+    while(v){
+        insere_no(g2, v->id_viz);
+        insere_aresta(g2,copia->id_no, v->id_viz);
+        v = v->prox_viz;
+    }
+    return g2;
 }
 
 TG* criaGrafo(char* nomeArq){					//le o arquivo e monta o grafo
@@ -213,8 +277,8 @@ TG* criaGrafo(char* nomeArq){					//le o arquivo e monta o grafo
 
     TG* g = cria();
 
-    for(i = 0; i< n; i++){						//... e insere n nos no grafo
-        insere_no(g,i+1);
+    for(i = n; i>0; i--){						//... e insere n nos no grafo
+        insere_no(g,i);
     }
 
     r = fscanf(arquivo,"%d%d",&n,&i);			//le as arestas...
